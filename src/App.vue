63f483589f7d7f2fd45bd34d469ea8b0159b7f5b
Condarnad <Condarnad@gmail.com>
2018-03-18 20:22:12 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <reservations swipeable/>
+</template>
+
+<script>
+import Reservations from './components/Reservations'
+
+export default {
+  name: 'App',
+  components: {
+      Reservations
+  }
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
